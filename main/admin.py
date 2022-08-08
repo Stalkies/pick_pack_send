4fc71478043aa35537eea9eec2d182e7521519dc
Stalkie <jakehuasen@gmail.com>
2022-08-08 22:56:59 +0200

    creating basic structure of models

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Client, Good, Cart, Photo
 # Register your models here.
+admin.site.register(Client)
+admin.site.register(Good)
+admin.site.register(Cart)
+admin.site.register(Photo)
--- main/models.py
+++ main/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Client(models.Model):
+    name = models.CharField(max_length=24)
+    phone = models.CharField(max_length=12)
+    def __str__(self):
+        return self.name
+    class Meta:
+        verbose_name_plural = "Клиент"
+class Cart(models.Model):
+    good_id = models.BigIntegerField('Товары', max_length=24)
+    client_id = models.BigIntegerField('Клиент', max_length=10)
+    class Meta:
+        verbose_name_plural = "Корзина"
+
+class Good(models.Model):
+    title = models.CharField('Название товара', max_length=256)
+    description = models.TextField('Описание товара', max_length=4069)
+    class Meta:
+        verbose_name_plural = "Товары"
+
+class Photo(models.Model):
+    url = models.CharField(max_length=512)
+    class Meta:
+        verbose_name_plural = "Фото"
