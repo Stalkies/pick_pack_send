d481991aabc933de5665d844c19174100ffc6a5d
Stalkie <jakehuasen@gmail.com>
2022-08-14 04:03:49 +0200

    creating basic html structure

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', views.show_index),
 ]
+urlpatterns += staticfiles_urlpatterns()
--- pick_pack_ship/settings.py
+++ pick_pack_ship/settings.py
@@ -15,8 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
+# Quick-start development settings - unsuitable for productioniko
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
@@ -116,7 +115,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'main/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
